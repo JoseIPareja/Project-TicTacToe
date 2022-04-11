de86a6705aa62b58c8be6063933f8a006aceafda
Jose Ignacio Pareja <jose.ignacio97@gmail.com>
2022-04-11 00:07:14 -0300

    Added Character Selection & Ready Buttons Logic in JS.

--- script.js
+++ script.js
@@ -1,13 +1,69 @@
-// OBJECT GAMEBOARD, that contains an array[]. Where X and O will gather.
-
-
-// OBJECT displayController, in order to control the flow of the game
-
-
 // FACTORY of PLAYERS:
-const factoryPlayers = (name) => {
-    const sayName = () => console.log(`I am ${name}`);
-    return {sayName};
+const playerFactory = (name, mark) => {
+    return {name, mark};
 }
 
-// write a function that will display the contents of the gameboard array to the webpage
+
+// CHARACTER SELECTION
+let ghosts1 = [];
+const p1sel1btn = document.querySelector('#p1sel1');
+const p1sel2btn = document.querySelector('#p1sel2');
+const p1sel3btn = document.querySelector('#p1sel3');
+
+p1sel1btn.addEventListener('click', () => {
+    ghosts1.push('p1sel1');
+})
+
+p1sel2btn.addEventListener('click', () => {
+    ghosts1.push('p1sel2');
+})
+
+p1sel3btn.addEventListener('click', () => {
+    ghosts1.push('p1sel3');
+})
+
+let ghosts2 = [];
+const p2sel1btn = document.querySelector('#p2sel1');
+const p2sel2btn = document.querySelector('#p2sel2');
+const p2sel3btn = document.querySelector('#p2sel3');
+
+p1sel1btn.addEventListener('click', () => {
+    ghosts2.push('p2sel1');
+})
+
+p1sel2btn.addEventListener('click', () => {
+    ghosts2.push('p2sel2');
+})
+
+p1sel3btn.addEventListener('click', () => {
+    ghosts2.push('p2sel3');
+})
+
+
+// READY BUTTONS ACTIONS
+const readyBtn1 = document.querySelector('.ready1');
+const readyBtn2 = document.querySelector('.ready2');
+
+readyBtn1.addEventListener('click', () => {
+    const input1 = document.querySelector('.nameinput1');
+    const player1 = playerFactory(input1.value, ghosts1.slice(-1)[0]);
+    console.log(player1.name);
+    console.log(player1.mark);
+})
+
+readyBtn2.addEventListener('click', () => {
+    const input2 = document.querySelector('.nameinput2');
+    const player2 = playerFactory(input2.value, ghosts2.slice(-1)[0]);
+    console.log(player2.name);
+    console.log(player2.mark);
+})
+
+//function play(e) {
+    //const cell = e.target;
+    //const currentClass = 
+//}
+
+//const cellElements = document.querySelectorAll('[data-cell]');
+//cellElements.forEach(cell => {
+    //cell.addEventListener('click', play, {once: true})
+//});
